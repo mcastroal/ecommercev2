aadfd8089a9d3ddae389f68931dad1822024725e
mcastroal <marilyn.castroalonso@gmail.com>
2026-02-18 04:20:46 -0500

    troubleshooting

--- server/db.js
+++ server/db.js
@@ -3,10 +3,17 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: Number(process.env.DB_PORT || 3306),
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 20000,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
